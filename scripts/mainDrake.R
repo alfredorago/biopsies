ce156fe37b31eb4dfd37373d9f9407f17a2f3271
Alfredo Rago <arago@nyx.binf.ku.dk>
2019-07-26 22:44:08 +0200

    Improved main utility script
    Now includes stderr and plotting function

--- scripts/mainDrake.R
+++ scripts/mainDrake.R
@@ -3,4 +3,9 @@ library(drake)
 library(here)
 
 r_outdated(r_args = list(stderr = here("tmp", "drakemistake.log")))
-r_make()
+r_make(r_args = list(stderr = here("tmp", "drakemistake.log")))
+
+# Plot the plan
+source(file = here("scripts", "Plan.R"))
+conf = drake_config(plan = plan)
+vis_drake_graph(conf)
