a955d90617f1d4fef1e76098a1d2c4038a61dae9
Alfredo Rago <arago@nyx.binf.ku.dk>
2019-07-26 20:28:29 +0200

    Plan now uses elegant pipes
    The locked scope errors were caused by functions within mutate referencing the original object

--- scripts/Plan.R
+++ scripts/Plan.R
@@ -2,17 +2,17 @@
 
 plan = drake_plan(
   gcnt.vst = DESeq2::varianceStabilizingTransformation(round(gcnt)),
-  gvst.df = reshape::melt(gcnt.vst),
-  gvst.df.mut = mutate(gvst.df,
-                       lib = str_replace(gvst.df$X2, "_(L|P).*", ""),
-                       cond = str_replace_all(str_extract(gvst.df$X2, "_(L|P)_"), "_", "")
-  ),
-  fcvst.df = reshape::cast(gvst.df.mut, X1+lib~cond, value="value"),
-  fcvst.df2 = mutate(fcvst.df,
-                     Lcnt = 2^L,
-                     Pcnt = 2^P,
-                     log2abd = log2(Lcnt + Pcnt),
-                     log2fc = L- P,
-                     part = str_replace(lib, ".*_", "")
-  )
+  gvst.df = reshape::melt(gcnt.vst) %>% 
+    mutate(.,
+           lib = str_replace(X2, "_(L|P).*", ""),
+           cond = str_replace_all(str_extract(X2, "_(L|P)_"), "_", "")
+    ),
+  fcvst.df = reshape::cast(gvst.df, X1+lib~cond, value="value") %>% 
+    mutate(.,
+           Lcnt = 2^L,
+           Pcnt = 2^P,
+           log2abd = log2(Lcnt + Pcnt),
+           log2fc = L - P,
+           part = str_replace(lib, ".*_", "")
+    )
 )
